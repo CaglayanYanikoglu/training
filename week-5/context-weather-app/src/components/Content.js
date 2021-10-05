f6cc8e5f5bf1e3e9e3eb04ec23db42b935e04c93
ogunakar9 <ogunakar09@gmail.com>
2021-10-05 22:11:29 +0300

    fix Izmir not showing up

--- week-5/context-weather-app/src/components/Content.js
+++ week-5/context-weather-app/src/components/Content.js
@@ -13,7 +13,7 @@ const Content = () => {
         image:
           "https://images.unsplash.com/photo-1611034542458-85aace78c787?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
       });
-    } else if (city === "İzmir") {
+    } else if (city === "Izmir") {
       setValue(city);
       setCurrentCity({
         name: "Izmir",
